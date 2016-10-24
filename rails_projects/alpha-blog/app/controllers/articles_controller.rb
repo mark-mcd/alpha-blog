a415fdcacd393a200c15b3b20847ce9257d1ce9a
mm <markjcamcd@gmail.com>
2016-10-24 21:18:55 +0000

    Add edit, show, index etc.

--- rails_projects/alpha-blog/app/controllers/articles_controller.rb
+++ rails_projects/alpha-blog/app/controllers/articles_controller.rb
@@ -1,7 +1,19 @@
 class ArticlesController < ApplicationController
+    def index
+       @articles = Article.all 
+       #NB plural. This grabs all articles for this action?
+    end
+    
     def new
         @article = Article.new
     end
+    #form submission then pushed to create aciton
+    
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     
     def create
         #render plain: params[:article].inspect
@@ -13,10 +25,21 @@ class ArticlesController < ApplicationController
             render :new
         end
     end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfllly created"
+            redirect_to article_path(@article)
+        else
+            render :edit
+        end
+    end
 
     def show
         @article = Article.find(params[:id])
     end
+    
     private
         def article_params
             params.require(:article).permit(:title, :description)
