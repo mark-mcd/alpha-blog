71d53740d0b799862116bae2aa746b6dccea9da0
mm <markjcamcd@gmail.com>
2017-01-14 17:52:47 +0000

    January return, plus debugs!

--- rails_projects/alpha-blog/app/controllers/articles_controller.rb
+++ rails_projects/alpha-blog/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :show, :destroy, :update]
+    
     def index
        @articles = Article.all 
        #NB plural. This grabs all articles for this action?
@@ -11,7 +13,6 @@ class ArticlesController < ApplicationController
     
     
     def edit
-        @article = Article.find(params[:id])
     end
 
     
@@ -27,7 +28,6 @@ class ArticlesController < ApplicationController
     end
     
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was successfllly created"
             redirect_to article_path(@article)
@@ -37,11 +37,10 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])
     end
+        
     
     def destroy
-       @article = Article.find(params[:id])
        @article.destroy
        flash[:notice] = "Article was successflly deleted"
     end
@@ -50,5 +49,8 @@ class ArticlesController < ApplicationController
         def article_params
             params.require(:article).permit(:title, :description)
         end
-
+        def set_article
+            @article = Article.find(params[:id])
+        end
+             
 end
